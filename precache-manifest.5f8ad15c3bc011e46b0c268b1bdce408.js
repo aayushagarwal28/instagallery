58cefd4ae2dba0229fb2b86b773644f444dc9527
aayush <aayush.agarwal28@gmail.com>
2021-10-12 22:15:39 +0530

    Updates

---
+++ precache-manifest.5f8ad15c3bc011e46b0c268b1bdce408.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82c2648104a0c268f8a5ff900d664153",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db7ce921833290db79c3",
+    "url": "/static/css/main.22e14d87.chunk.css"
+  },
+  {
+    "revision": "d8c9a4e5e6dad95c4504",
+    "url": "/static/js/2.cfa0b57b.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/static/js/2.cfa0b57b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db7ce921833290db79c3",
+    "url": "/static/js/main.93b015e7.chunk.js"
+  },
+  {
+    "revision": "f35af6f7ccead2fe5dbb",
+    "url": "/static/js/runtime-main.4422d9b2.js"
+  }
+]);
